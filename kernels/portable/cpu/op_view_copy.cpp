ce4ac1b383b88f781ce2312638db86777060a848
BujSet <ranganath1000@gmail.com>
2025-07-30 12:51:43 -0700

    Simple branch to repro dtype selective build error

--- kernels/portable/cpu/op_view_copy.cpp
+++ kernels/portable/cpu/op_view_copy.cpp
@@ -34,7 +34,6 @@ Tensor& view_copy_out(
       InvalidArgument,
       out);
 
-
   // Resize for dynamic shape
   ET_KERNEL_CHECK_MSG(
       ctx,
@@ -50,8 +49,6 @@ Tensor& view_copy_out(
 
   ET_KERNEL_CHECK(ctx, tensor_is_default_dim_order(self), InvalidArgument, out);
 
-  static constexpr const char op_name[] = "view_copy.out";
-
   ET_KERNEL_CHECK(
       ctx, check_view_copy_args(self, size_int64_t, out), InvalidArgument, out);
 
@@ -59,7 +56,6 @@ Tensor& view_copy_out(
     memcpy(out.mutable_data_ptr(), self.const_data_ptr(), self.nbytes());
   }
   return out;
-
 }
 
 } // namespace native
